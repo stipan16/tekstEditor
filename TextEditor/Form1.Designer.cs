a9c40fa9eb67559db183208e9f053161e431f72e
stipan16 <slausi00@fesb.hr>
2015-08-31 18:08:36 +0200

    first

---
+++ TextEditor/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace TextEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fIleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtVelikiTextBox = new System.Windows.Forms.RichTextBox();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.potražiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtReplace = new System.Windows.Forms.TextBox();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.searchColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.replaceColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fIleToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(511, 24);
+            this.menuStrip1.Stretch = false;
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fIleToolStripMenuItem
+            // 
+            this.fIleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fIleToolStripMenuItem.Name = "fIleToolStripMenuItem";
+            this.fIleToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fIleToolStripMenuItem.Text = "FIle";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cutToolStripMenuItem,
+            this.copyToolStripMenuItem,
+            this.pasteToolStripMenuItem,
+            this.undoToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.potražiToolStripMenuItem,
+            this.fontToolStripMenuItem,
+            this.backgroundColorToolStripMenuItem,
+            this.textColorToolStripMenuItem,
+            this.searchColorToolStripMenuItem,
+            this.replaceColorToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // txtVelikiTextBox
+            // 
+            this.txtVelikiTextBox.Location = new System.Drawing.Point(12, 62);
+            this.txtVelikiTextBox.Name = "txtVelikiTextBox";
+            this.txtVelikiTextBox.Size = new System.Drawing.Size(487, 255);
+            this.txtVelikiTextBox.TabIndex = 1;
+            this.txtVelikiTextBox.Text = "";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // cutToolStripMenuItem
+            // 
+            this.cutToolStripMenuItem.Name = "cutToolStripMenuItem";
+            this.cutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cutToolStripMenuItem.Text = "Cut";
+            this.cutToolStripMenuItem.Click += new System.EventHandler(this.cutToolStripMenuItem_Click);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.pasteToolStripMenuItem.Text = "Paste";
+            this.pasteToolStripMenuItem.Click += new System.EventHandler(this.pasteToolStripMenuItem_Click);
+            // 
+            // undoToolStripMenuItem
+            // 
+            this.undoToolStripMenuItem.Name = "undoToolStripMenuItem";
+            this.undoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.undoToolStripMenuItem.Text = "Undo";
+            this.undoToolStripMenuItem.Click += new System.EventHandler(this.undoToolStripMenuItem_Click);
+            // 
+            // potražiToolStripMenuItem
+            // 
+            this.potražiToolStripMenuItem.Name = "potražiToolStripMenuItem";
+            this.potražiToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.potražiToolStripMenuItem.Text = "Search";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(436, 3);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(330, 6);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 3;
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.fontToolStripMenuItem.Text = "Font";
+            this.fontToolStripMenuItem.Click += new System.EventHandler(this.fontToolStripMenuItem_Click);
+            // 
+            // backgroundColorToolStripMenuItem
+            // 
+            this.backgroundColorToolStripMenuItem.Name = "backgroundColorToolStripMenuItem";
+            this.backgroundColorToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.backgroundColorToolStripMenuItem.Text = "Background Color";
+            this.backgroundColorToolStripMenuItem.Click += new System.EventHandler(this.backgroundColorToolStripMenuItem_Click);
+            // 
+            // textColorToolStripMenuItem
+            // 
+            this.textColorToolStripMenuItem.Name = "textColorToolStripMenuItem";
+            this.textColorToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.textColorToolStripMenuItem.Text = "Text Color";
+            this.textColorToolStripMenuItem.Click += new System.EventHandler(this.textColorToolStripMenuItem_Click);
+            // 
+            // txtReplace
+            // 
+            this.txtReplace.Location = new System.Drawing.Point(330, 33);
+            this.txtReplace.Name = "txtReplace";
+            this.txtReplace.Size = new System.Drawing.Size(100, 20);
+            this.txtReplace.TabIndex = 4;
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(437, 33);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(75, 23);
+            this.btnReplace.TabIndex = 5;
+            this.btnReplace.Text = "Replace";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // searchColorToolStripMenuItem
+            // 
+            this.searchColorToolStripMenuItem.Name = "searchColorToolStripMenuItem";
+            this.searchColorToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.searchColorToolStripMenuItem.Text = "Search Color";
+            this.searchColorToolStripMenuItem.Click += new System.EventHandler(this.searchColorToolStripMenuItem_Click);
+            // 
+            // replaceColorToolStripMenuItem
+            // 
+            this.replaceColorToolStripMenuItem.Name = "replaceColorToolStripMenuItem";
+            this.replaceColorToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.replaceColorToolStripMenuItem.Text = "Replace Color";
+            this.replaceColorToolStripMenuItem.Click += new System.EventHandler(this.replaceColorToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 329);
+            this.Controls.Add(this.btnReplace);
+            this.Controls.Add(this.txtReplace);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtVelikiTextBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Text Editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fIleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox txtVelikiTextBox;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem undoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem potražiToolStripMenuItem;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backgroundColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem textColorToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtReplace;
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.ToolStripMenuItem searchColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem replaceColorToolStripMenuItem;
+    }
+}
+
---
+++ TextEditor/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TextEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> lines;
+        private Color colorSearch=Color.Yellow;
+        private Color colorReplace=Color.Red;
+
+        //klikom na New očistiti sadržaj velikog text boxa
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtVelikiTextBox.Clear();
+        }
+
+        //klikom na Exit zatvara se app
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //klikom na Save otvara se SaveFileDialog
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            //svojstva saveFileDialoga
+            saveFileDialog.Title = "Save as";
+            saveFileDialog.FileName = "";
+            saveFileDialog.Filter = "Text files (*.rtf,*txt)|*.rtf;*txt" ;
+
+            //ako je korisnik kliknia OK
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string path = saveFileDialog.InitialDirectory + saveFileDialog.FileName;
+             
+                try
+                {
+                    //Stream writer čita liniju po liniju iz velikog text boxa i upisuje je u datoteku definiranu path-om 
+                    using (StreamWriter sw = new StreamWriter(path))
+                    {
+                        foreach (var line in txtVelikiTextBox.Lines)
+                        {
+                            if(line!=null)
+                                sw.WriteLine(line);
+                        }
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("nešto nije u redu sa putanjom");
+                }
+            
+            }
+        }
+        
+        
+        //klikom na Open otvara se OpenFileDialog
+       private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            //svojstva openFileDialoga
+            openFileDialog.Title = "Open";
+            openFileDialog.FileName = "";
+            openFileDialog.Filter = "Text files (*.rtf, *.txt)|*.rtf;*.txt";
+
+            //ako je korisnik kliknia OK
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog.InitialDirectory + openFileDialog.FileName;
+
+                //Stream Reader čita liniju po liniju iz datoteke definirane path-om i zapisuje linije u listu  
+                
+                try
+                {
+                    using (StreamReader sr = new StreamReader(path))
+                    {
+                        lines = new List<string>();
+                        while (sr.Peek() >= 0)
+                        {
+                            string line = sr.ReadLine();
+                            lines.Add(line);
+                        }
+                        //svaku liniju zapisuje u veliki text box
+                        foreach (var l in lines)
+                        {
+                            txtVelikiTextBox.Text += l.ToString() + "\n";
+                        }
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("nešto nije u redu sa putanjom");
+                }
+            }
+        }
+
+        //klik na Cut
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //provjera da li je označen tekst
+            if (txtVelikiTextBox.SelectedText != "")
+            {
+                //reže označeni tekst i sprema ga u ClipBoard
+                txtVelikiTextBox.Cut();
+            }
+
+        }
+
+        //klik na Copy
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //provjera da li je označen text
+            if (txtVelikiTextBox.SelectionLength > 0)
+            {
+                //kopira označeni tekst u Clipboard
+                txtVelikiTextBox.Copy();
+            }
+        }
+
+        //klik na Paste
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //provjera da li postoji tekst u Clipboardu koji bi se zalijepio u text box
+            if (Clipboard.GetDataObject().GetDataPresent(DataFormats.Text) == true)
+            {
+                ///provjera da li je označen neki tekst unutar text boxa
+                if (txtVelikiTextBox.SelectionLength > 0)
+                {
+                    //upit da li želimo novi tekst zalijepiti preko trenutno označenog
+                    if (MessageBox.Show("Da li želiš zalijepiti preko označenog?", "", MessageBoxButtons.YesNo) == DialogResult.No)
+                    {
+                        //preskoći označeni tekst
+                        txtVelikiTextBox.SelectionStart = txtVelikiTextBox.SelectionStart + txtVelikiTextBox.SelectionLength;
+                    }
+                }
+                txtVelikiTextBox.Paste();
+            }
+            
+        }
+
+        //klik na Undo
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //provjera da li se zadnja operacija može poništiti
+            if (txtVelikiTextBox.CanUndo == true)
+            {
+                //poništi zadnju operaciju
+                txtVelikiTextBox.Undo();
+
+                //očiti buffer kako se slučajno poništena akcija nebi opet izvela
+                txtVelikiTextBox.ClearUndo();
+            }
+        }
+
+        //klik na font
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                //font tekst
+                txtVelikiTextBox.Font = fontDialog.Font;
+            }
+        }
+
+        //klik na Background color
+        private void backgroundColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                //boja pozadine
+                txtVelikiTextBox.BackColor = colorDialog.Color;
+            }
+        }
+
+        //klik na text color
+        private void textColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                //boja slova
+                txtVelikiTextBox.ForeColor = colorDialog.Color;
+            }
+        }
+
+        //klik na botun search
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            int index = 0;
+            //praznimo i ponovno vračamo sadržaj velikog text boxa,kako bi se osigurali da nisu ostale oznake prethodne pretrage
+            String temp = txtVelikiTextBox.Text;
+            txtVelikiTextBox.Text = "";
+            txtVelikiTextBox.Text = temp;
+            while (index < txtVelikiTextBox.Text.LastIndexOf(txtInput.Text))
+            {
+                //traži tekst unutar zadanog raspona
+                txtVelikiTextBox.Find(txtInput.Text, index, txtVelikiTextBox.TextLength, RichTextBoxFinds.None);
+                //obojamo pozadinu pronađenog teksta
+                txtVelikiTextBox.SelectionBackColor = colorSearch;
+                //inklementiraj index kako bi se potraga nastavila
+                index = txtVelikiTextBox.Text.IndexOf(txtInput.Text, index) + 1;
+            }
+        }
+
+        //klik na botun replace
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            if (txtInput.Text != "" && txtReplace.Text != "")
+            {
+                int index = 0;
+                //praznimo i ponovno vračamo sadržaj velikog text boxa,kako bi se osigurali da nisu ostale oznake prethodne pretrage
+                String temp = txtVelikiTextBox.Text;
+                txtVelikiTextBox.Text = "";
+                txtVelikiTextBox.Text = temp;
+                while (index < txtVelikiTextBox.Text.LastIndexOf(txtInput.Text))
+                {
+                    //traži tekst unutar zadanog raspona
+                    txtVelikiTextBox.Find(txtInput.Text, index, txtVelikiTextBox.TextLength, RichTextBoxFinds.None);
+                    //obojamo pozadinu pronađenog teksta
+                    txtVelikiTextBox.SelectionBackColor = colorReplace;
+                    
+                    txtVelikiTextBox.SelectedText = txtReplace.Text;
+                    //inklementiraj index kako bi se potraga nastavila
+                    index = txtVelikiTextBox.Text.IndexOf(txtReplace.Text, index) + 1;
+                }
+            }
+            else
+            {
+                if(txtInput.Text=="")
+                    MessageBox.Show("molim unesite tekst koji želite zamjeniti");
+                
+                else if(txtReplace.Text=="")
+                    MessageBox.Show("molim unesite text kojim će se postojeći zamjeniti");
+                
+                else
+                    MessageBox.Show("morate ispuniti oba textboxa");
+
+            }
+        }
+
+        //klik na Search color
+        private void searchColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                //boja pozadine teksta koji se pronađe
+                colorSearch = colorDialog.Color; 
+            }
+        }
+
+        //klik na Replace Color
+        private void replaceColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                //boja pozadine teksta koji se zamijeni
+                colorReplace = colorDialog.Color;
+            }
+        }
+
+    }
+}
